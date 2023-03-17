64b3ce27eca0d729a4476d6f44f78dcec713e563
Hassnae BADDOU <samya1902.com>
2023-03-17 03:43:20 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
