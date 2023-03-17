a18b4dda4a126d24e1b442d7053a87b9b24da5d4
Hassnae BADDOU <samya1902.com>
2023-03-17 03:57:22 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	putchar ('\n');
+	return (0);
+}
