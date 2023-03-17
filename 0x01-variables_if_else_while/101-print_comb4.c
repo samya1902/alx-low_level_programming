84afea65ef6ce842761054c818ded130d1c9d53b
Hassnae BADDOU <samya1902.com>
2023-03-17 10:52:23 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,8 +21,11 @@ int main(void)
 				putchar(a + '0');
 				putchar(b + '0');
 				putchar(c + '0');
-				putchar(',');
-				putchar(' ');
+				if (a != 7 || b != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 				c++;
 			}
 			b++;
