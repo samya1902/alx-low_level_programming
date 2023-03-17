0dbbb48a6e4ec8323cfcbf76c4b4929a407d57ff
Hassnae BADDOU <samya1902.com>
2023-03-17 08:53:55 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		putchar(a + '0');
+		if (a != 9)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
