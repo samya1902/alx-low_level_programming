b4e64d09959e3c8df02bca30daafb797b44a82a9
Hassnae BADDOU <samya1902.com>
2023-03-21 15:32:43 -0700

    to 98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first integer to be added to b
+ * @b: second integer to be added to a
+ * Retrun: integer sum of int a and int b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry
+ * Return: always 0
+ */
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(111);
+	return (0);
+}
