86741b78ffc41240ec9dce51bfa6b375c1c9544c
Hassnae BADDOU <samya1902.com>
2023-03-21 13:29:02 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @n: a number
+ * Return: the absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r;
+
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	return (0);
+}
