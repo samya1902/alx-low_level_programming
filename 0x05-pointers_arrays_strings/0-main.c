4eaee39aa14caea282330799b5247edadddc19e7
Hassnae BADDOU <samya1902.com>
2023-03-27 04:20:10 -0700

    98

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
