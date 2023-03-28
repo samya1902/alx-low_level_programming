1d818bb8065029ba331ec9435cee03e3172209cf
Hassnae BADDOU <samya1902.com>
2023-03-28 05:11:41 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string input
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int l;
+	int n;
+	int i;
+
+	while (*str != '\0')
+	{
+		str++;
+		l++;
+	}
+	l--;
+	if (l % 2 != 0)
+	{
+		n = (l - 1) / 2;
+	}
+	else
+		n = l / 2;
+	for (i = n; i < l; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
