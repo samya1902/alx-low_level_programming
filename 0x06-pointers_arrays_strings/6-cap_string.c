dc26efa25f4321346f7f5b8ea36279bd0a701cd0
Hassnae BADDOU <samya1902.com>
2023-03-30 07:05:06 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @n: string
+ * Return: pointer
+ */
+char *cap_string(char *n)
+{
+	int i = 0;
+
+	while (n[i])
+	{
+		while (!(n[i] >= 'a' && n[i] <= 'z'))
+			i++;
+		if (n[i - 1] == ' ' || n[i - 1] ==  '\t' || n[i - 1] == '\n' || n[i - 1] == ',' || n[i - 1] == ';' || n[i - 1] == '.' || n[i - 1] == '!' || n[i - 1] == '?' || n[i - 1] == '"' || n[i - 1] == '(' || n[i - 1] == ')' || n[i - 1] == '{' || n[i - 1] == '}' || i == 0)
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
