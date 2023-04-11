b2c9bfe45f49531c03450bb996f4e8225de23cd6
Hassnae BADDOU <samya1902.com>
2023-04-11 05:38:39 -0700

    2 args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
