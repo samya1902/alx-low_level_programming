f0d15eea904a0861c45b1e62aee75c0f623b571a
Hassnae BADDOU <samya1902.com>
2023-04-17 04:43:05 -0700

    struct dog

---
+++ 0x0E-structures_typedef/0-main.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
