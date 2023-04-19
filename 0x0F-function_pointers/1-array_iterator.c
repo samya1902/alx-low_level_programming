d1c65780a1b978c103f0a058025dabce859ba475
Hassnae BADDOU <samya1902.com>
2023-04-19 04:50:34 -0700

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * array_iterator - executes a function given as a parameter 
+ * array_iterator - executes a function given as a parameter
  * @array: array
  * @size: how many elem to print
  * @action: pointer to print in regular or hex
