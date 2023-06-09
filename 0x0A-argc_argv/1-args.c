6586db7adba0a9f7e258cddd2830a3c236bda706
BADDOU Hassnae <samya1902@gmail.com>
2023-06-09 03:05:30 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * @argc: arguments count
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
