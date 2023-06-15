cea389c1096f035a60ebcccf5a350c38c7b0a148
BADDOU Hassnae <samya1902@gmail.com>
2023-06-15 14:52:18 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,5 +8,12 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
 }
