0297211c406ab0503e37d8560e26b625a82bc785
Hassnae BADDOU <samya1902@gmail.com>
2023-08-03 10:33:25 -0700

    dec

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,11 +9,11 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int dec = 0;
 
-	if(!b)
+	if (!b)
 		return (0);
-	for(i = 0; b[i]; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if(b[i] < '0' || b[i] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		dec = 2 * dec + (b[i] - '0');
 	}
