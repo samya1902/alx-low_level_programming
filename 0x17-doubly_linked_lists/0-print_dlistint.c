67c0fc9af32b9b0a31d179167bdb8a4cb3e13702
Hassnae BADDOU <samya1902@gmail.com>
2023-08-17 03:00:50 -0700

    0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: head
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int nb = 0;
+
+	if (h == NULL)
+		return (nb);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		nb++;
+		h = h->next;
+	}
+	return (nb);
+}
